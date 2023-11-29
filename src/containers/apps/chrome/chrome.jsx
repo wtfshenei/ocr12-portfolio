cb8106b45e66905d540b739d5926f032f954207b
Ludovic Parriaud <wtfshenei@gmail.com>
2023-11-29 18:34:53 +0100

    feat(App/Chrome): Logique navigation projets + WiP

--- src/containers/apps/chrome/chrome.jsx
+++ src/containers/apps/chrome/chrome.jsx
@@ -1,25 +1,99 @@
-import React from 'react';
-import {BookmarksBar, ContentContainer, HeaderNavigator, MainBar} from "./chrome.styles";
+import React, {useState} from 'react';
+import {BookmarkButton, BookmarksBar, ButtonNavigator, ContentContainer, ContentNavigator, HeaderNavigator, LeftMainBar, Loading, LoadingContainer, MainBar, RightMainBar} from "./chrome.styles";
 import Button from "../../../components/button/button";
 import Input from "../../../components/input/input";
+import ReactIcon from "../../../components/utils/icons/react/reactIcon";
+import ArrowLeft from "../../../components/utils/icons/webBrowser/arrowLeft";
+import ArrowRight from "../../../components/utils/icons/webBrowser/arrowRight";
+import Reload from "../../../components/utils/icons/webBrowser/reload";
+import House from "../../../components/utils/icons/webBrowser/house";
+import Javascript from "../../../components/utils/icons/javascript/javascript";
 
 const Chrome = () => {
+    const projects = [
+        {
+            id: 'home',
+            title: 'Home',
+            icon: <House />,
+            content: 'Contenu de la Home'
+        },
+        {
+            id: 'project1',
+            title: 'Projet 1',
+            icon: <Javascript />,
+            content: 'Contenu de Projet 1'
+        },
+        {
+            id: 'project2',
+            title: 'Projet 2',
+            icon: <ReactIcon />,
+            content: 'Contenu de Projet 2'
+        }
+    ]
+
+    const [currentProject, setCurrentProject] = useState(projects[0])
+    const [currentIndex, setCurrentIndex] = useState(0)
+    const [isLoading, setIsLoading] = useState(false)
+
+    const handleBookmarkClick = (index) => {
+        setIsLoading(true)
+
+        setTimeout(() => {
+            setCurrentProject(projects[index])
+            setCurrentIndex(index)
+            setIsLoading(false)
+        }, 1000)
+    }
+
+    const handleArrowLeftClick = () => {
+        if (currentIndex > 0) {
+            setCurrentIndex(currentIndex - 1)
+            setCurrentProject(projects[currentIndex - 1])
+        }
+    }
+
+    const handleArrowRightClick = () => {
+        if (currentIndex < projects.length - 1) {
+            setCurrentIndex(currentIndex + 1)
+            setCurrentProject(projects[currentIndex + 1])
+        }
+    }
+
     return (
         <ContentContainer>
             <HeaderNavigator>
                 <MainBar>
-                    <Button>A</Button>
-                    <Button>-></Button>
-                    <Button>C</Button>
-                    <Input></Input>
+                    <LeftMainBar>
+                        <ButtonNavigator onClick={handleArrowLeftClick} disabled={currentIndex === 0}><ArrowLeft /></ButtonNavigator>
+                        <ButtonNavigator onClick={handleArrowRightClick} disabled={currentIndex === projects.length - 1}><ArrowRight /></ButtonNavigator>
+                        <ButtonNavigator><Reload /></ButtonNavigator>
+                    </LeftMainBar>
+                    <Input disabled value={`http://localhost:3000/${currentProject.title}`}></Input>
+                    <RightMainBar>
+                        <Button><ReactIcon /></Button>
+                    </RightMainBar>
                 </MainBar>
                 <BookmarksBar>
-                    <Button>Home</Button>
-                    <Button>Projet 1</Button>
-                    <Button>Projet 2</Button>
-                    <Button>Projet 3</Button>
+                    {projects.map((project, index) => (
+                        <BookmarkButton key={project.id} onClick={() => handleBookmarkClick(index)} isActive={currentIndex === index}>
+                            {project.icon}
+                            {project.title}
+                        </BookmarkButton>
+                    ))}
                 </BookmarksBar>
             </HeaderNavigator>
+            <ContentNavigator>
+                {isLoading ? (
+                    <LoadingContainer>
+                        <Loading />
+                    </LoadingContainer>
+                ) : (
+                    <>
+                        <h2>{currentProject.title}</h2>
+                        <p>{currentProject.content}</p>
+                    </>
+                )}
+            </ContentNavigator>
         </ContentContainer>
     );
 };
--- src/containers/apps/chrome/chrome.styles.js
+++ src/containers/apps/chrome/chrome.styles.js
@@ -1,8 +1,11 @@
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
+import Button from "../../../components/button/button";
 
 export const ContentContainer = styled.div`
   display: flex;
+  flex-direction: column;
   width: 100%;
+  height: 100%;
   margin: 0;
   padding: 0;
 `
@@ -23,10 +26,52 @@ export const MainBar = styled.div`
   padding: 0 10px;
   
   input {
-    width: 80%;
+    width: 70%;
     height: 30px;
     border-radius: 30px;
     border: none;
+    padding: 0 15px;
+  }
+  
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 80%;
+    width: 35px;
+    background: none;
+    border: none;
+    
+    &:hover {
+      border-radius: 50px;
+      background-color: rgba(255, 255, 255, 0.3);
+    }
+  }
+`
+export const LeftMainBar = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 100%;
+  margin-right: 15px;
+  
+  svg {
+    height: 15px;
+    width: 15px;
+    fill: ${({ disabled }) => disabled ? 'grey' : 'white' }
+  }
+`
+export const RightMainBar = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 100%;
+  margin-left: 15px;
+  
+  svg {
+    height: 20px;
+    width: 20px;
+    fill: dodgerblue;
   }
 `
 export const BookmarksBar = styled.div`
@@ -36,4 +81,90 @@ export const BookmarksBar = styled.div`
   width: 100%;
   height: 30px;
   background-color: cadetblue;
+  padding: 0 15px;
+`
+export const ContentNavigator = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+`
+const spin = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`
+export const Loading = styled.div`
+  border: 4px solid darkgrey;
+  border-top: 4px solid cornflowerblue;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  animation: ${spin} 1s linear infinite;
+`
+export const LoadingContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+`
+export const ButtonNavigator = styled(Button)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 80%;
+  background: none;
+  border: none;
+  margin-right: 5px;
+  color: white;
+
+  &:hover {
+    border-radius: 50px;
+    background-color: rgba(255, 255, 255, 0.3);
+  }
+
+  svg {
+    height: 15px;
+    width: 15px;
+    fill: ${({ disabled }) => disabled ? 'darkgrey' : 'white' }
+  }
+`
+export const BookmarkButton = styled(Button)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 80%;
+  background: none;
+  border: none;
+  margin-right: 5px;
+  color: white;
+
+  ${({ isActive }) => isActive && `
+    background-color: lightblue;
+    border-radius: 50px;
+    color: black;
+    font-weight: bold;
+    
+    svg {
+      fill: black;  
+    }
+  `}
+
+  &:hover {
+    border-radius: 50px;
+    background-color: rgba(255, 255, 255, 0.3);
+  }
+
+  svg {
+    display: flex;
+    align-items: center;
+    height: 13px;
+    width: 13px;
+    margin-right: 5px;
+    fill: white;
+  }
 `
