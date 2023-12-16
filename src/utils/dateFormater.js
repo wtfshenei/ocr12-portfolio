ae4bc4009b36731b98c8046d847879212e75012b
Ludovic Parriaud <wtfshenei@gmail.com>
2023-12-16 21:53:55 +0100

    feat(Fonction): Affichage date depuis timestamp

---
+++ src/utils/dateFormater.js
@@ -0,0 +1,11 @@
+export const dateFormater = (timestamp) => {
+    const date = new Date(timestamp)
+    return date.toLocaleDateString('fr-FR', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    })
+}
