d13df1c80f32932ec03ede4224d78fedde41ce4d
Ludovic Parriaud <wtfshenei@gmail.com>
2024-02-06 21:22:09 +0100

    fix(Mobile): Déplacement Context en Mobile

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import {Provider} from "react-redux";
 import {store} from "./app/store";
-import {MobileProvider} from "./utils/MobileContext";
+import {MobileProvider} from "./mobile/utils/MobileContext";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
