3d353164030f10664599a04598862d88c096ccf2
Ludovic Parriaud <wtfshenei@gmail.com>
2024-02-15 20:33:30 +0100

    feat(Carousel): MaJ

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -1,25 +1,29 @@
 import React, {useState} from 'react';
+import AngleLeft from "../utils/icons/angles/angleLeft";
+import AngleRight from "../utils/icons/angles/angleRight";
 
-const Carousel = ({ projects }) => {
+const Carousel = ({ pictures, className }) => {
     const [currentIndex, setCurrentIndex] = useState(0)
 
     const goToPrevious = () => {
         const isFirstImg = currentIndex === 0
-        const newIndex = isFirstImg ? projects.content.length - 1 : currentIndex - 1
+        const newIndex = isFirstImg ? pictures.length - 1 : currentIndex - 1
         setCurrentIndex(newIndex)
     }
 
     const goToNext = () => {
-        const isLastImg = currentIndex === projects.content.length - 1
+        const isLastImg = currentIndex === pictures.length - 1
         const newIndex = isLastImg ? 0 : currentIndex + 1
         setCurrentIndex(newIndex)
     }
 
     return (
-        <div>
-            <button onClick={goToPrevious}>P</button>
-            <img src={projects.content[currentIndex].pictures} alt="Test"/>
-            <button onClick={goToNext}>S</button>
+        <div className={className}>
+            <button className={'carousel-button left'} onClick={goToPrevious}><AngleLeft /></button>
+            {pictures && pictures.length > 0 && (
+                <img src={pictures[currentIndex]} alt={`Slide ${currentIndex + 1}`}/>
+            )}
+            <button className={'carousel-button right'} onClick={goToNext}><AngleRight /></button>
         </div>
     );
 };
