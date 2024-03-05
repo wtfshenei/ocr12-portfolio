7ec90943a8891a4cc354032f1dd095254a35d07a
Ludovic Parriaud <wtfshenei@gmail.com>
2024-03-05 16:41:47 +0100

    feat(app/VSCode): Contenu

--- src/containers/apps/vscode/vscode.jsx
+++ src/containers/apps/vscode/vscode.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {ContentContainer, ContentLeftWrapper, ContentRightWrapper, MenuItemsLeft, RowItems, RowItemsTitle} from "./vscode.styled";
+import {ContentContainer, ContentLeftWrapper, ContentRightWrapper, LinksContainer, LinkWrapper, MenuItemsLeft, PictureContainer, RowItems, RowItemsTitle, TxtsContainer} from "./vscode.styled";
 import AngleDown from "../../../components/utils/icons/angles/angleDown";
 import AngleRight from "../../../components/utils/icons/angles/angleRight";
 import Folder from "../../../components/utils/icons/folder/folder";
@@ -7,6 +7,12 @@ import ReactIcon from "../../../components/utils/icons/react/reactIcon";
 import Javascript from "../../../components/utils/icons/javascript/javascript";
 import CheckContent from "../../../components/checkContent/checkContent";
 import {useMobile} from "../../../mobile/utils/MobileContext";
+import PictureLP from "./photo.jpg"
+import Github from "./assets/social/github/github";
+import Linkedin from "./assets/social/linkedin/linkedin";
+import Mail from "./assets/social/mail/mail";
+import Cv from "./assets/social/cv/cv";
+import CvDl from "./assets/social/cv/CV_de_Ludovic_Parriaud.pdf"
 
 const Vscode = () => {
     const isMobile = useMobile()
@@ -30,27 +36,64 @@ const Vscode = () => {
                             <Folder/>
                             <p>components</p>
                         </RowItems>
-                        <RowItems className={'child3 item-menu'}>
+                        <a href="#aboutme"><RowItems className={'child3 item-menu'}>
                             <ReactIcon/>
                             <p>aboutMe.jsx</p>
-                        </RowItems>
-                        <RowItems className={'child3 item-menu'}>
+                        </RowItems></a>
+                        <a href="#studies"><RowItems className={'child3 item-menu'}>
                             <ReactIcon/>
                             <p>studies.jsx</p>
-                        </RowItems>
-                        <RowItems className={'child3 item-menu'}>
+                        </RowItems></a>
+                        <a href="#career"><RowItems className={'child3 item-menu'}>
                             <ReactIcon/>
                             <p>career.jsx</p>
-                        </RowItems>
-                        <RowItems className={'child3 item-menu'}>
+                        </RowItems></a>
+                        <a href="#profile"><RowItems className={'child3 item-menu'}>
                             <Javascript fill={'yellow'}/>
                             <p>picture.styled.js</p>
-                        </RowItems>
+                        </RowItems></a>
                     </MenuItemsLeft>
                 </ContentLeftWrapper>
             </CheckContent>
             <ContentRightWrapper>
-                <h2>RIGHT</h2>
+                <PictureContainer>
+                    <img src={PictureLP} id={'profile'} alt="Moi"/>
+                </PictureContainer>
+                <LinksContainer>
+                    <a href={"https://github.com/wtfshenei/"} target={"_blank"} rel={"noreferrer"}><LinkWrapper>
+                        <Github />
+                        <span>Github</span>
+                    </LinkWrapper></a>
+                    <a href={"https://www.linkedin.com/in/ludovic-parriaud-4254002b8/"} target={"_blank"} rel={"noreferrer"}><LinkWrapper>
+                        <Linkedin/>
+                        <span>LinkedIn</span>
+                    </LinkWrapper></a>
+                    <a href={"mailto:ludovic.parriaud@gmail.com"} target={"_blank"} rel={"noreferrer"}><LinkWrapper>
+                        <Mail/>
+                        <span>Email</span>
+                    </LinkWrapper></a>
+                    <a href={CvDl} target={"_blank"} rel={"noreferrer"}><LinkWrapper>
+                        <Cv/>
+                        <span>CV</span>
+                    </LinkWrapper></a>
+                </LinksContainer>
+                <TxtsContainer>
+                    <p id={'aboutme'}>
+                        <span>Bonjour.</span> Je m'appelle Ludovic Parriaud et j'ai 36 ans. Je vis en Bourgogne et j'ai choisi de me reconvertir comme développeur web après avoir passé plus de 18 ans en tant qu'agent de maintenance ferroviaire à la SNCF. J'ai toujours
+                        eu un attrait et une fascination pour le développement, partir de rien et voir un projet prendre vie sous ses yeux est un sentiment incroyable.
+                    </p>
+                    <p id={'studies'}>
+                        <span>Etudes.</span> Apprenti en alternance de 2003 à 2005, j'ai obtenu mon BEP MSMA (Maintenance des Systèmes Mécaniques Automatisés) qui m'a ensuite permi de me faire embaucher au Technicentre de Nevers, l'établissement où j'étais affilié.
+                    </p>
+                    <p>
+                        <span>Formation.</span> J'ai débuté ma formation de reconversion en novembre 2022 jusqu'en mars 2024.
+                    </p>
+                    <p id={'career'}>
+                        <span>Parcours professionnel.</span> Pour le moment mon parcours professionnel se résume à mon expérience au sein de la SNCF. J'ai pu y exercer plusieurs corps de métiers dans lesquels j'ai appris à travailler aussi bien en collaboration
+                        qu'en totale autonomie. J'ai toujours fait en sorte d'apprendre un maximum concernant mes affectations afin de pouvoir vulgariser mon savoir et de former mes nouveaux collègues. Au dela des habilitations propres aux métiers que j'exerçais, j'ai
+                        toujours été également intéressé par le cadre de vie dans lequel j'évoluais, ce qui m'a, par exemple, amené à devenir Sauveteur-Secouriste du Travail.
+                    </p>
+                </TxtsContainer>
             </ContentRightWrapper>
         </ContentContainer>
     )
--- src/containers/apps/vscode/vscode.styled.js
+++ src/containers/apps/vscode/vscode.styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import {theme} from "../../../assets/styles/theme.styles";
 
 export const ContentContainer = styled.div`
     display: flex;
@@ -14,7 +15,6 @@ export const ContentLeftWrapper = styled.div`
     max-width: ${({isMobile}) => isMobile ? 'none' : '250px'};
     width: ${({isMobile}) => isMobile ? '100%' : '25%'};
     height: 100%;
-    background-color: bisque;
     padding: 0.25em;
 `
 export const ContentRightWrapper = styled.div`
@@ -22,7 +22,12 @@ export const ContentRightWrapper = styled.div`
     flex-direction: column;
     width: 100%;
     height: 100%;
-    background-color: burlywood;
+    padding: 1em;
+    gap: 2em;
+    
+    span {
+        font-weight: bold;
+    }
 `
 export const RowItemsTitle = styled.div`
     display: flex;
@@ -50,7 +55,6 @@ export const RowItems = styled.li`
     align-items: center;
     gap: 0.35em;
     height: 1.5em;
-    cursor: pointer;
     
     .js-color {
         fill: yellow;
@@ -87,5 +91,80 @@ export const MenuItemsLeft = styled.ul`
 
     .child3 {
         margin: 0 3.5em;
+    }
+
+    a {
+        text-decoration: none;
+        color: inherit;
+        
+        &:hover, &:visited, &:active {
+            text-decoration: none;
+            color: inherit;
+        }
+    }
+`
+export const PictureContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    img {
+        width: 250px;
+        height: 250px;
+        object-fit: cover;
+        border-radius: 50%;
+        border: 2px solid #fff;
+        box-shadow: 0 0 15px rgba(0, 0, 0, 0.6);
+        transition: transform 0.3s ease;
+        
+        &:hover {
+            transform: scale(1.05);
+            box-shadow: 0 0 20px rgba(0, 0, 0, 0.8);
+        }
+    }
+`
+export const TxtsContainer = styled.div`
+    p {
+        margin-bottom: 1.5em;
+    }
+`
+export const LinksContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 1.5em;
+    width: 100%;
+`
+export const LinkWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    width: 3em;
+    height: 3em;
+    transition: transform 0.3s ease;
+    position: relative;
+    
+    svg {
+        width: 100%;
+        height: 100%;
+        fill: ${theme.colors.color1};
+        transition: opacity 0.3s ease;
+    }
+    
+    span {
+        position: absolute;
+        bottom: -2em;
+        width: 100%;
+        text-align: center;
+        font-size: 0.75em;
+        color: ${theme.colors.color1};
+    }
+
+    &:hover {
+        transform: scale(1.05);
+        box-shadow: 0 0 20px rgba(100,149,237, 0.8);
     }
 `
